00ad975591aa04620f581d98eb61f3955220e353
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-03-22 19:07:56 -0700

    sum of ints

---
+++ Lecture1/mathoptimized.py
@@ -0,0 +1,8 @@
+## Using mathmatical optimization to turn an O(n) operation to an O(1) operation
+# The operation is adding up all the eklements in a list
+
+def sumofint(int):
+    n = int
+    return n * (n + 1) / 2
+
+print(sumofint(5))
